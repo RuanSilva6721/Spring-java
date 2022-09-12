6371171682eede33632b9bb45c124d9791ec6b02
RuanFelipe <ruansilva6721@gmail.com>
2022-09-12 09:39:45 -0300

    iniciando repo spring

---
+++ src/main/java/br/com/RuanFelipe/SpringRuanFelipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.RuanFelipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRuanFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRuanFelipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/RuanFelipe/SpringRuanFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.RuanFelipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRuanFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
